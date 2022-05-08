5ac3810f1259e7bf101f70c01759f3d4462e71d0
AleksandraLewandowska <69048@student.ue.poznan.pl>
2022-05-08 20:26:30 +0200

    newton v1

---
+++ main.py
@@ -0,0 +1,12 @@
+import math as m
+def Newton(n,k):
+  wynik= m.factorial(n) / (m.factorial(k)*m.factorial(n-k))
+  return wynik
+n_k= input()
+wartosci=n_k.split(" ")
+n=int(wartosci[0])
+k=int(wartosci[1])
+if k == 0 or k == n : 
+  print('1')
+else : 
+  print(Newton(n,k))
